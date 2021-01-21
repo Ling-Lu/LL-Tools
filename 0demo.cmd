3071410bf66fa28294ce346207fabfe4fa7428d2
Ling-Lu <262177478@qq.com>
2021-01-21 18:25:18 +0800

    update bat demo

--- 0demo.cmd
+++ 0demo.cmd
@@ -1,33 +1,28 @@
 @echo off
-set startTips====================================================
-echo %startTips%
 echo.
+set startTips=--------------------------------------------------------------------------
+echo %startTips%
+set run_failure=0
 REM -----------------------------------------------------------------------------------------------Code
+echo Hello My world!
 call :usage
-goto error
+call :error
+
+
+
+
+
+
 
 
 
 REM -----------------------------------------------------------------------------------------------Code
-
-REM -----------------------------------------------------------------------------------------------Error handle
-REM error tips
-goto end
-:error
-echo Wrong package name
-goto end
-:error_pm_more
-echo more than one package matched,input more accurate package name please
-goto end
-:error_package_dead
-echo process of %package now alive
-
-REM -----------------------------------------------------------------------------------------------Error handle
-
 
 :end
-call endTips "%startTips%" 1 
-call colorStr "%endTips%"
+set colorAttr=9F
+if %run_failure% EQU 1 set colorAttr=CF
+call endTips "%startTips%" 1 -
+call colorStr "%endTips%" 2 %colorAttr%
 goto :eof
 
 REM -----------------------------------------------------------------------------------------------Functions
@@ -36,6 +31,21 @@ REM Functions below
 call colorStr "Usage" 2 79
 call colorStr "Write your help Message here" 2
 goto :eof
+
+REM error tips
+:error
+echo Wrong package name
+set run_failure=1
+goto :eof
+:error_pm_more
+echo more than one package matched,input more accurate package name please
+set run_failure=1
+goto :eof
+:error_package_dead
+echo process of %package now alive
+set run_failure=1
+goto :eof
+
 REM -----------------------------------------------------------------------------------------------Functions
 
 
